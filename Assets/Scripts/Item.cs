272d95ee5ea6217a849fca93c66f70e75283770d
Your Name Here <aitorpalomares@hotmail.com>
2014-04-22 16:19:54 +0100

    Revert "Revert "first upload""
    This reverts commit 85f8c6c16e4708aaaabfa3f0866ff5aeca43bc67.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	public int pointsValue = 5;
+	public AudioClip ItemSound;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.tag == "Player"){
+			AudioSource.PlayClipAtPoint(ItemSound, Camera.main.transform.position, 1);
+			NotificationCenter.DefaultCenter().PostNotification(this, "IncrementPoints", pointsValue);
+			Destroy(gameObject);
+		}
+	}
+}
